7bfc349387d4286f4c5907b4441ab759a6e78c35
renancalvis <renancalvis@gmail.com>
2026-07-31 04:39:59 -0400

    chore: dockerfile front and back update for prod

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "179.198.99.46",
+    "179.198.99.46:3100",
+    "localhost",
+    "localhost:3100",
+  ],
 };
 
 export default nextConfig;
